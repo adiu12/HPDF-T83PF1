69b5ec577d9867b4aa68dabf6ffd80bef264128f
adiu12 <adit.mcet@gmail.com>
2018-02-02 23:44:49 +0530

    Commitg

---
+++ index.js
@@ -0,0 +1,15 @@
+import { AppRegistry } from 'react-native';
+//import App from './App';
+//import search from './search';
+import React, { Component } from 'react';
+import wit from './wit';
+// import Login from './Login';
+ 
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+AppRegistry.registerComponent('myapp', () => wit);
